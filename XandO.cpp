20ef0e555fbfb35bdbe2d1dc063717c2a7f96206
xReggimon <r-mcmullen.1@onu.edu>
2024-04-28 14:09:16 -0400

    Completed functions, might need slightly altered

--- XandO.cpp
+++ XandO.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -22,7 +23,7 @@ bool hasThreeInRow(char board[], char playerMark);
 //vertically, horizontally, or diagonally.
 
 void displayBoard(char board[]);
-//Prints out the current board; the board display must be as shown above.
+//Prints out the current board
 
 void displayGameStats(int ties, int player1Score, int player2Score);
 //Prints out the “stats” for all the games : number of games tied, number of games player 1 won, and number of
@@ -30,4 +31,74 @@ void displayGameStats(int ties, int player1Score, int player2Score);
 
 int main() {
 
+	string player1;string player2;
+	int ties = 0; int player1Score = 0; int player2Score = 0;
+	char board[9] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+	cout << "The legendary Tic Tac Toe." <<endl;
+	cout << endl;
+	cout << "Enter usernames to start :) " << endl;
+	cout << "player1: "; getline(cin, player1);
+	cout << "player2: "; getline(cin, player2);
+	cout << endl;
+
+
+	displayBoard(board);
+	//displayGameStats(ties, player1Score, player2Score, player1, player2);
+	return 0;
+}
+
+
+int getPlayerInput(string playerName) {
+	int input;
+	cout << playerName << ", please enter your move (1-9): ";
+	cin >> input;
+	return input;
+}
+
+//This function is used to determine whether the user is putting in a valid Tic Tac Toe input
+bool isLegalMove(char board[], int location) {
+	return (location >= 1 && location <= 9 && board[location - 1] != 'X' && board[location - 1] != 'O');
+}
+
+void placeMarkOnBoard(char board[], char playerMark, int location) {
+	board[location - 1] = playerMark;
+}
+
+void clearBoard(char board[]) {
+	for (int i = 0; i < 9; i++) {
+		board[i] = '1' + i;
+	}
+}
+
+bool hasThreeInARow(char board[], char playerMark) {
+	for (int i = 0; i < 3; i++) {
+		if (board[i] == playerMark && board[i + 3] == playerMark && board[i + 6] == playerMark) {
+			return true;
+		}
+	}
+	for (int i = 0; i < 3; i++) {
+		if (board[i] == playerMark && board[i * 3+1] == playerMark && board[i *3 + 2] == playerMark) {
+			return true;
+		}
+	}
+	if ((board[0] == playerMark && board[4] == playerMark && board[8] == playerMark) ||
+		(board[2] == playerMark && board[4] == playerMark && board[6] == playerMark)) {
+		return true;
+	}
+	return false;
+}
+
+void displayBoard(char board[]) {
+	cout << " " << board[0] << " | " << board[1] << " | " << board[2] << endl;
+	cout << "-----------" << endl;
+	cout << " " << board[3] << " | " << board[4] << " | " << board[5] << endl;
+	cout << "-----------" << endl;
+	cout << " " << board[6] << " | " << board[7] << " | " << board[8] << endl;
+}
+
+void displayGameStats(int ties, int player1Score, int player2Score, string player1, string player2) {
+	cout << "Ties: " << ties << endl;
+	cout << player1 << "Wins: " << player1Score << endl;
+	cout << player2 << "Wins: " << player2Score << endl;
 }
